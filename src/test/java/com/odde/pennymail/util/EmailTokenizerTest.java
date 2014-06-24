5b712735ddf8d34440d3a962f3e82f21e3e2ef63
ARCHER <ARCHER@earth.local>
2014-06-24 14:39:19 +0700

    Email Tokenizer

---
+++ src/test/java/com/odde/pennymail/util/EmailTokenizerTest.java
@@ -0,0 +1,32 @@
+package com.odde.pennymail.util;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class EmailTokenizerTest {
+		EmailTokenizer testMail = new EmailTokenizer();
+	
+	@Test
+	public void whiteSpaceBetweenEmail() {
+		String[] expected = {"a@b.com","aa@bb.com","aaa@bbb.com"};
+		String[] A = testMail.splitEmail("a@b.com , aa@bb.com , aaa@bbb.com ");
+		assertArrayEquals(expected, A);
+	}
+	
+	@Test
+	public void nullValueBetweenEmail() {
+		String[] expected = {"a@b.com","aa@bb.com","aaa@bbb.com"};
+		String[] A = testMail.splitEmail("a@b.com , aa@bb.com ,  , aaa@bbb.com ");
+		assertArrayEquals(expected, A);
+	}
+	
+	@Test
+	public void nullInput() {
+		String[] expected = {};
+		String[] A = testMail.splitEmail("");
+		assertArrayEquals(expected, A);
+	}	
+
+}
