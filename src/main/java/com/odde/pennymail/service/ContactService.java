58dc79e2b4bdea4142e1a8d6e67d9bfa69bb600d
MARCUS <MARCUS@earth.local>
2014-06-25 14:37:23 +0700

    Create Model: retireve emails from list

--- src/main/java/com/odde/pennymail/service/ContactService.java
+++ src/main/java/com/odde/pennymail/service/ContactService.java
@@ -1,14 +1,14 @@
 package com.odde.pennymail.service;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class ContactService {
-
+	ArrayList<String> emaillist = new ArrayList<String>();
 	public List list() {
-		return null;
+		return emaillist;
 	}
 
 	public void add(String email) {
+		emaillist.add(email);
 	}
-
 }
