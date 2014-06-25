767392f8b6ddb9571dbd2c838fa06858a94b1aa0
KHAN <KHAN@earth.local>
2014-06-25 13:45:51 +0700

    map request for method add to /addrecipient

--- src/main/java/com/odde/pennymail/controller/ContactController.java
+++ src/main/java/com/odde/pennymail/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.odde.pennymail.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import com.odde.pennymail.service.ContactService;
 @Controller
 public class ContactController {
 
+	@Autowired
 	private ContactService contactService;
 
 	@RequestMapping(value = "/contact", method = RequestMethod.GET)
@@ -27,6 +29,7 @@ public class ContactController {
 		this.contactService = service;
 	}
 
+	@RequestMapping(value = "/addrecipient", method = RequestMethod.POST)
 	public ModelAndView add(String email) {
 		this.contactService.add(email);
 		ModelAndView modelAndView = new ModelAndView("contact");
