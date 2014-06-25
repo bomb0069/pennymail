7915c67b85675e5a8fc53797de2a767cd0433f36
KHAN <KHAN@earth.local>
2014-06-25 15:32:16 +0700

    check email is not null

--- src/test/java/com/odde/pennymail/controller/ContactControllerTest.java
+++ src/test/java/com/odde/pennymail/controller/ContactControllerTest.java
@@ -72,5 +72,13 @@ public class ContactControllerTest {
 				"contactList");
 		assertEquals(oldContactList.size() + 2, newContactList.size());
 	}
-
+	
+	@Test
+	public void emptyEmail() {
+		ModelAndView modelAndView = controller.add(null);
+		
+		ArrayList newContactList = (ArrayList) modelAndView.getModel().get(
+				"contactList");
+		assertEquals(0, newContactList.size());
+	}
 }
