5091c69a1dc740ac9bead2f57ce9b73db3bc158c
MARCUS <MARCUS@earth.local>
2014-06-25 16:45:26 +0700

    Add sorting test case

--- src/main/java/com/odde/pennymail/service/ContactService.java
+++ src/main/java/com/odde/pennymail/service/ContactService.java
@@ -1,5 +1,7 @@
 package com.odde.pennymail.service;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -8,10 +10,14 @@ import org.springframework.stereotype.Service;
 public class ContactService {
 	ArrayList<String> emaillist = new ArrayList<String>();
 	public List list() {
+		Collections.sort(emaillist);
 		return emaillist;
 	}
 
 	public void add(String email) {
+		if(!email.trim().equals("")){
 		emaillist.add(email);
+		}
 	}
+	
 }
